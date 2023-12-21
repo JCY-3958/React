2f58cf4c17a906e80d449a3057c035919f899b20
JCY-3958 <w7e43958@naver.com>
2023-12-21 16:53:58 +0900

    리액트 첫날 5장 까지

--- src/chpater-5/CommentList.jsx
+++ src/chpater-5/CommentList.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+  {
+    name: "전찬용",
+    comment: "집가고 싶다.",
+  },
+  {
+    name: "이성종",
+    comment: "발더게 하고싶다.",
+  },
+  {
+    name: "박진호",
+    comment: "퇴사하고 싶다.",
+  }
+];
+
 function CommentList(props) {
   return (
     <div>
-      <Comment name={"이인재"} comment={"안녕하세요, 힘듭니다."} />
-      <Comment name={"리액트"} comment={"좋은거 맞음?"} />
+      {comments.map((comment) => {
+        return (
+          <Comment name={comment.name} comment={comment.comment} />
+        );
+      })}
+      {/* <Comment name={"이인재"} comment={"안녕하세요, 힘듭니다."} /> */}
+      {/* <Comment name={"리액트"} comment={"좋은거 맞음?"} /> */}
     </div>
   );
 }
